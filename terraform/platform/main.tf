83c09b91d224c479f703fd337c5fc6098ca0e823
mauriba <53594538+mauriba@users.noreply.github.com>
2024-11-24 20:01:02 +0100

    feat: allow icmp communication for testing purposes
    also refactors security rules to be contained in the platform NSG block

--- terraform/platform/main.tf
+++ terraform/platform/main.tf
@@ -12,37 +12,43 @@ resource "azurerm_network_security_group" "platform" {
   name                = "nsg-snet-192-168-20-0-24-platform"
   location            = azurerm_resource_group.platform_network.location
   resource_group_name = azurerm_resource_group.platform_network.name
-}
 
-# region platform network security rules
-resource "azurerm_network_security_rule" "allow_outbound_internet" {
-  name                        = "allow-outbound-internet"
-  resource_group_name         = azurerm_resource_group.platform_network.name
-  network_security_group_name = azurerm_network_security_group.platform.name
-  priority                    = 200
-  direction                   = "Outbound"
-  access                      = "Allow"
-  protocol                    = "*"
-  source_port_range           = "*"
-  destination_port_range      = "*"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-}
+  security_rule {
+    name                       = "allow-all-outbound"
+    priority                   = 200
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 
-resource "azurerm_network_security_rule" "deny_all_inbound" {
-  name                        = "deny_all_inbound"
-  resource_group_name         = azurerm_resource_group.platform_network.name
-  network_security_group_name = azurerm_network_security_group.platform.name
-  priority                    = 300
-  direction                   = "Inbound"
-  access                      = "Deny"
-  protocol                    = "*"
-  source_port_range           = "*"
-  destination_port_range      = "*"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
+  security_rule {
+    name                       = "deny_all_inbound"
+    priority                   = 300
+    direction                  = "Inbound"
+    access                     = "Deny"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "allow-ping-inbound"
+    priority                   = 290
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Icmp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
-# endregion
 
 resource "azurerm_virtual_network" "platform" {
   name                = "vnet-192-168-20-0-24-${var.location}"
